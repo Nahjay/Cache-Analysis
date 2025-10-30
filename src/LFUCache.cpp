e41f4649614fa0a2891fc2f8f2e09296fa7ef175
Nahjay Battieste <nahjaybattieste@gmail.com>
2025-10-29 21:53:22 -0400

    feat: implementing basic func so far for LFU

--- src/LFUCache.cpp
+++ src/LFUCache.cpp
@@ -1,3 +1,24 @@
 #include "LFUCache.h"
 
 // Implementation for LFUCache Class
+
+// Constructor
+LFUCache::LFUCache(int capacity) {
+    // Set capacity
+    this->capacity = capacity;
+}
+
+// Reset
+void LFUCache::reset() {
+    // Reset both maps by clearing them
+    this->cacheMap.clear();
+    this->freqMap.clear();
+}
+
+// Destructor
+LFUCache::~LFUCache() {
+    // Just call reset when LFUCache object falls out of scope
+    // again redundant cuz those maps will be cleaned up anyways
+    // but good practice/design :)
+    reset();
+}
